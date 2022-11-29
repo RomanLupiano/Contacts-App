000de2e6c4d45d3d94121760dfe29fae2af8447c
RomanLupiano <romanlupiano@gmail.com>
2022-11-29 15:22:34 -0300

    User-contacts relation

--- sql/setup.sql
+++ sql/setup.sql
@@ -17,9 +17,8 @@ INSERT INTO users (name, email, password) VALUES ("Marcos", "romanlupiano@gmail.
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    phone_number VARCHAR(15)
-);
+    user_id INT NOT NULL,
+    phone_number VARCHAR(15),
 
-INSERT INTO contacts (name, phone_number) VALUES ("Pepe", "1234567890");
-INSERT INTO contacts (name, phone_number) VALUES ("Marcos", "1234111111");
-INSERT INTO contacts (name, phone_number) VALUES ("Roman", "1234222222");
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
